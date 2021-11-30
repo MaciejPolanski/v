9aceb5ebfb4b6b3a313d75aa99f25b4187ca8c2b
Maciej Polanski <mega@shodan-mint>
2021-11-30 02:53:06 +0100

    First comparions of std::vectors

--- main.cpp
+++ main.cpp
@@ -6,78 +6,107 @@
 
 using std::cout;
 using std::setw;
+using std::to_string;
 
-const int elemNum = 1000;      // How many element push into vector
-const std::size_t vecNum = 50;  // How many vectors interaced
+const int elemNum{ 10000};      // How many element push into vector
+const std::size_t vecNum{100};  // How many pararell vectors in each step 
 static_assert(vecNum > 0);
 struct blob {                   // Block-Of-Bytes to be used 
-    unsigned char blob[512];
+    unsigned char blob[1024];
 };
 
-const int logN = 41;           // How many inspections during test
-static_assert(logN > 1);
 struct oneLog{
-    std::size_t id;
+    std::size_t size;
     std::size_t capacity;
     void* ptr;
-    int cntResize;
-    int cntMoves;
 
-    oneLog(): cntResize(0), cntMoves(0){}
+    //oneLog(): cntResize(0), cntMoves(0){}
 };
-std::array<oneLog, logN> logs;
+std::array<oneLog, elemNum> logs;
 
 template<typename T_vector, std::size_t M>
-int test(std::array<T_vector, M>& testVectors)
+int test(int N,                                // N is # of push_backs
+         std::array<T_vector, M>& testVectors, // Vectors can be preinitialized 
+         std::array<oneLog, elemNum>& logs)    // Per-loop measurments
 {
-    int logCount = 0;
-    const int logStep = elemNum / (logN -1);
-    logs.fill({});
-    void *oldPtr = &*testVectors[0].begin();
-
-    for(int i =0; i < elemNum; ++i){
+    for(int i =0; i < N; ++i){
         for(auto& v: testVectors){
             v.emplace_back(blob{});
         }
-        if (oldPtr != &*testVectors[0].begin()) {   
-            ++logs[logCount].cntResize;
-            logs[logCount].cntMoves += testVectors[0].size();
-            oldPtr = &*testVectors[0].begin();
-        }
-        if (i+1 >= logStep * logCount) {
-            logs[logCount].id = testVectors[0].size();
-            logs[logCount].capacity = testVectors[0].capacity();
-            logs[logCount].ptr = &*testVectors[0].begin();
-            ++logCount;
-        }
+        logs[i].size = testVectors[0].size();
+        logs[i].capacity = testVectors[0].capacity();
+        logs[i].ptr = &*testVectors[0].begin();
     }
     return 1;
+}
+
+std::string mem2str(long m)
+{
+    if ( abs(m / 1024 / 1024 / 1024) > 0 )
+        return to_string(m/1024/1024/1024) + " GB";
+    if ( abs(m / 1024 / 1024) > 0 )
+        return to_string(m/1024/1024) + " MB";
+    if ( abs(m / 1024) > 0 )
+        return to_string(m/1024) + " KB";
+    return to_string(m) + "  B";
+}
+
+void showLog(int testN, std::array<oneLog, elemNum>& logs)
+{
+    cout << setw(8) << "size()" << "  " << setw(8) << "capacity()  &*.begin()     diff_to_previous\n";
+    void *oldPtr{0};
+    for(int i = 0; i < testN; ++i){
+        auto& log = logs[i];
+        if (oldPtr == log.ptr)
+            continue;
+        long ptrdiff = (char*)log.ptr - (char*)oldPtr;
+        // cout << setw(2) << i << ", ";
+        cout << setw(8) << log.size << ", " << setw(10) << mem2str(sizeof(blob) * log.capacity) << "  ";
+        cout << log.ptr << " diff " << setw(10) << mem2str(ptrdiff) <<"\n";
+        oldPtr = log.ptr;
+    }
+    cout << "Data size: " << mem2str(sizeof(blob) * testN * vecNum) << ", ";
+    cout << elemNum << " elements of size " << sizeof(blob) << " in " << vecNum << " vectors\n";
 }
 
 int main(int argc, char** argv)
 {
     std::array<std::vector<blob>, vecNum> vectors1;
 
+    cout << "Empty std::vector, one row per reallocation\n";
+    int testN{elemNum};
     std::chrono::steady_clock clk;
     auto clkS = clk.now();
-    test(vectors1);
+    test(testN, vectors1, logs);
     auto clkE = clk.now();
 
     std::chrono::milliseconds mili;
     mili  = std::chrono::duration_cast<std::chrono::milliseconds>(clkE - clkS);
+    showLog(testN, logs);
+    cout << "Elapsed: " << mili.count() << "ms\n";
 
-    int totResizes{0};
-    int totMoves{0};
-    for(int i = 0; i < logN; ++i){
-        totResizes += logs[i].cntResize;
-        totMoves += logs[i].cntMoves;
-        // cout << setw(2) << i << ", ";
-        cout << setw(8) << logs[i].id << ", " << setw(8) << sizeof(blob) * logs[i].capacity / 1024 << " KB ";
-        cout << logs[i].ptr << " " << setw(2) << totResizes <<  setw(8) << totMoves << "\n";
-    }
-    cout << elemNum << " elements of size " << sizeof(blob) << " in " << vecNum << " vectors\n";
-    cout << "Data size: " << sizeof(blob) * elemNum * vecNum / 1024 / 1024 << " MB";
-    cout << ", elapsed: " << mili.count() << "ms\n";
-    //cout << decltype(dur)::period::num << ":" << decltype(dur)::period::den << "\n" ;;
+    // Reused vectors
+    cout << "Reused std::vector, no reallocations - one row\n";
+    for(auto& v: vectors1) v.clear();
+    clkS = clk.now();
+    test(testN, vectors1, logs);
+    clkE = clk.now();
+
+    mili  = std::chrono::duration_cast<std::chrono::milliseconds>(clkE - clkS);
+    showLog(testN, logs);
+    cout << "Elapsed: " << mili.count() << "ms\n";
+
+    // Reserved vectors
+    cout << "Reserved std::vector\n";
+    std::array<std::vector<blob>, vecNum> v;
+    vectors1.swap(v);
+    for(auto& v: vectors1) v.reserve(testN);
+    clkS = clk.now();
+    test(testN, vectors1, logs);
+    clkE = clk.now();
+
+    mili  = std::chrono::duration_cast<std::chrono::milliseconds>(clkE - clkS);
+    showLog(testN, logs);
+    cout << "Elapsed: " << mili.count() << "ms\n";
 }
 
